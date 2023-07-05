cceac6d61916721cbbac069726f5be2f32b5112b
Ravi1522 <rshankar1081@gmail.com>
2023-07-06 02:47:44 +0530

    Changes done in script and readMe file

--- README.md
+++ README.md
@@ -14,3 +14,18 @@ Here are some ideas to get you started:
 - 😄 Pronouns: ...
 - ⚡ Fun fact: ...
 -->
+
+## Running the Java code
+#### Prerequisite: Ensure you have Java Development Kit (JDK) installed. You can download it from [Oracle's JDK website](https://www.oracle.com/java/technologies/javase-jdk16-downloads.html).
+
+##### Follow these steps to execute the Java Programme:
+1. Create a Folder in your Local System.
+2. Open a command prompt or terminal and navigate to the folder which u created 
+3. Type below command
+   ```bash
+   git Clone [Repo link] https://github.com/Ravi1522/Ravi1522.git  
+4. Once clonning is done, Import your project in Eclipse IDE
+5. Go To "TestRunner" Class and right click --> click on Run As --> Java Application .
+6. Now we can see the output in Consol
+
+
--- src/com/simple/package1/RecentPlayed.java
+++ src/com/simple/package1/RecentPlayed.java
@@ -1,15 +1,19 @@
 package com.simple.package1;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.junit.Assert;
+
 public class RecentPlayed {
 
 	private int intialCapacity;
+	boolean limitFlag = false;
 	Map<String, String> map = new HashMap<String, String>();
 
 	public RecentPlayed(int intialCapacity) {
@@ -17,60 +21,85 @@ public class RecentPlayed {
 	}
 
 	public void addSong(String song, String user) {
-		
+
 		List list2 = new ArrayList();
 		map.put(song, user);
-		Iterator<Map.Entry<String, String>> itr1 = map.entrySet().iterator();
-		while (itr1.hasNext()) {
-			Map.Entry<String, String> entry = itr1.next();
-			String usr = entry.getValue();
-			list2.add(usr);
-		}		
-		int repeatedCount = repeatedUser(list2);
-		removeSong(repeatedCount);
-		
+		if (!map.isEmpty()) {
+			Iterator<Map.Entry<String, String>> itr1 = map.entrySet().iterator();
+			while (itr1.hasNext()) {
+				Map.Entry<String, String> entry = itr1.next();
+				String usr = entry.getValue();
+				list2.add(usr);
+			}
+		} else {
+			Assert.fail();
+		}
+		String userName = checkRepeatedUser(list2);
+		if (limitFlag)
+			removeSong(userName);
 	}
+
 	public List getRecentlyPlayed(String user) {
 
 		List list = new ArrayList();
 		String song = null;
 		String userName;
-		Iterator<Map.Entry<String, String>> itr = map.entrySet().iterator();
-		while (itr.hasNext()) {
-			Map.Entry<String, String> entry = itr.next();
-			song = entry.getKey();
-			userName = entry.getValue();
-			if (userName.equals(user)) {
-				list.add(song);
+		try {
+			Iterator<Map.Entry<String, String>> itr = map.entrySet().iterator();
+			while (itr.hasNext()) {
+				Map.Entry<String, String> entry = itr.next();
+				song = entry.getKey();
+				userName = entry.getValue();
+				if (userName.equals(user)) {
+					list.add(song);
+				}
 			}
+		} catch (Exception e) {
+			Assert.fail();
 		}
 		return list;
 	}
 
-	public void removeSong(int count) {
+	public void removeSong(String user) {
 
-		if (count > intialCapacity) {
-			Set set= map.keySet();
-			Iterator itr = set.iterator();
-			String key=(String)itr.next();
-			map.remove(key);
+		try {
+			Iterator<Map.Entry<String, String>> itr = map.entrySet().iterator();
+			while (itr.hasNext()) {
+				Map.Entry<String, String> entry = itr.next();
+				String key = entry.getKey();
+				String value = entry.getValue();
+				if (value.equals(user)) {
+					map.remove(key);
+					break;
+				}
+			}
+		} catch (Exception e) {
+			Assert.fail();
 		}
 	}
-	
-	public int repeatedUser(List list) {
-	
-		int count=1;
-		String arr[]= new String[list.size()];
-		for (int k=0; k<list.size();k++) {
-			arr[k]=(String) list.get(k);
+
+	public String checkRepeatedUser(List list) {
+
+		int count = 1;
+		String user = null;
+		int i, j;
+		String arr[] = new String[list.size()];
+		for (int k = 0; k < list.size(); k++) {
+			arr[k] = (String) list.get(k);
 		}
-		for (int i=0;i<arr.length;i++) {
-			for (int j=i+1;j<arr.length;j++) {
-				if (arr[i]==arr[j]) {
+		for (i = 0; i < arr.length; i++) {
+			for (j = i + 1; j < arr.length; j++) {
+				if (arr[i] == arr[j]) {
 					count++;
 				}
 			}
+			if (count > intialCapacity) {
+				user = arr[i];
+				limitFlag = true;
+				break;
+			}
+			count = 1;
 		}
-		return count;
+		return user;
 	}
 }
